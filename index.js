725a51ec6b14b86c0caa1a7ad1f5d83ca9f899d8
zachariahcraven <118693079+zachariahcraven@users.noreply.github.com>
2024-07-12 09:57:14 -0600

    user interface button for changing grid size

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
+let userGridSize = 2;
+let divs = new Array(userGridSize*userGridSize); //array of divs to add to grid
 
 const containor = document.querySelector("main");
 const grid = document.createElement("grid");
 
-let arr = new Array(256);
-for (let i = 0; i < arr.length; i++) {
-    arr[i] = document.createElement("div");
-    arr[i].addEventListener("mouseover", () => {
-        arr[i].style.background = "blue";
-    })
-}
+const newGridSize = document.createElement("button");
+newGridSize.textContent = "New Grid Size";
+newGridSize.addEventListener("mousedown", () => {
+    userGridSize = prompt("Enter a number for the size of the grid.");
+    //remove old grid
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].remove();
+    }
+    newGridSize.remove();
+    //create new divs
+    divs = new Array(userGridSize*userGridSize);
+    for (let i = 0; i < divs.length; i++) {
+        divs[i] = document.createElement("div");
+        divs[i].addEventListener("mouseover", () => {
+            divs[i].style.background = "blue";
+        })
+        divs[i].style.flex = (" 1 1 " + ((1/userGridSize)*100)) + "%";
+    }
+    //append new nodes
+    for (let i = 0; i < divs.length; i++) {
+        grid.appendChild(divs[i]);
+    }
+    containor.appendChild(grid);
+    containor.appendChild(newGridSize);
+});
 
+
+for (let i = 0; i < divs.length; i++) {
+    divs[i] = document.createElement("div");
+    divs[i].addEventListener("mouseover", () => {
+        divs[i].style.background = "blue";
+    })
+    divs[i].style.flex = (" 1 1 " + ((1/userGridSize)*100)) + "%";
+}
 //append nodes ------------------------------
-for (let i = 0; i < arr.length; i++) {
-    grid.appendChild(arr[i]);
+for (let i = 0; i < divs.length; i++) {
+    grid.appendChild(divs[i]);
 }
 containor.appendChild(grid);
-
-
+containor.appendChild(newGridSize);
